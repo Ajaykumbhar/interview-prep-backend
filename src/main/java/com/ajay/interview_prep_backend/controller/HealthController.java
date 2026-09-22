32e49b6ece7c7ae034077e37f3e55e6da439cdd3
Ajay kumbhar <ajaykumbhar891@gmail.com>
2026-09-22 22:52:37 +0530

    Add DTO responses and request parameter examples

--- src/main/java/com/ajay/interview_prep_backend/controller/HealthController.java
+++ src/main/java/com/ajay/interview_prep_backend/controller/HealthController.java
@@ -1,7 +1,10 @@
 package com.ajay.interview_prep_backend.controller;
 
+import com.ajay.interview_prep_backend.dto.ApiResponse;
 import com.ajay.interview_prep_backend.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +14,18 @@ public class HealthController {
     public HealthController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
+    @GetMapping("/health/{name}")
+    public ApiResponse hello(@PathVariable String name) {
+        return new ApiResponse("Success", greetingService.getGreeting() + " " +name );
+    }
+
     @GetMapping("/health")
-    public String health(){
-        return greetingService.getGreeting();
+    public ApiResponse health() {
+        return new ApiResponse("Success", greetingService.getGreeting());
+    }
+
+    @GetMapping("/greet")
+    public ApiResponse greet(@RequestParam String name) {
+        return new ApiResponse("Success", greetingService.getGreeting() + " ::: " +name );
     }
 }
---
+++ src/main/java/com/ajay/interview_prep_backend/dto/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.ajay.interview_prep_backend.dto;
+
+public class ApiResponse {
+    private String  status;
+    private String message;
+
+    public ApiResponse(String status, String message){
+        this.status=status;
+        this.message=message;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
+
+}
